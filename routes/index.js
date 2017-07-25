1fc89cb149ea1c51c6915a67ecfc8f531094f892
Joel Dinis <joeldinisei1@gmail.com>
2017-07-25 20:53:12 +0200

    change

--- routes/index.js
+++ routes/index.js
@@ -94,9 +94,7 @@ app.post('/api/login', function(req, res, next) {
 
          database.login(username,password)
                 .then(function (user_id) {
-                    res.status(200).json({
-                                message: "SUCCESS"
-                            });
+                   res.status(200).send(user_id.rows);
                 })
 
 
