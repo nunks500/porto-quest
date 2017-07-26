5e80782fb8e68a3d458607896fb517acf38f1594
Joel Dinis <joeldinisei1@gmail.com>
2017-07-26 16:56:03 +0200

    tentativa

--- routes/index.js
+++ routes/index.js
@@ -111,8 +111,15 @@ app.options('/api/*', function(req, res, next) {
     
          database.getObjectives()
                 .then(function (user_id) {           
-                                console.log("nunu");
-                               res.writeHead(200, {'Access-Control-Allow-Origin: *','Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'});
+                                var headers = {};
+      // IE8 does not allow domains to be specified, just the *
+      // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+      headers["Access-Control-Allow-Origin"] = "*";
+      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+      headers["Access-Control-Allow-Credentials"] = false;
+      headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+                               res.writeHead(200, headers);
                             });
                
 
