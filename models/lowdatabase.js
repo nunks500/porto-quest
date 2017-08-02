2a261be569ad7e4d8273183ea1241c1ce5d64484
Joel Dinis <joeldinisei1@gmail.com>
2017-08-02 17:25:32 +0200

    cenas

--- models/lowdatabase.js
+++ models/lowdatabase.js
@@ -140,7 +140,7 @@ client.connect((err) => {
 
     exports.getobjbyid = function (id) {
        return new Promise(function (resolve, reject) {
-                          client.query("SELECT unnest(x) from (SELECT objectid FROM objetivoscompletos WHERE utilizadorid = $1 as x)",[id],
+                          client.query("SELECT unnest(x) from (SELECT objectid as x FROM objetivoscompletos WHERE utilizadorid = $1)",[id],
                           function (err, result) {
                                     if (err) {
                                         reject(err);
