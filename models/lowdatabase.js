eeb5d0244172578d372e368446fe54a7c0e1626e
Joel Dinis <joeldinisei1@gmail.com>
2017-08-02 16:56:49 +0200

    try

--- models/lowdatabase.js
+++ models/lowdatabase.js
@@ -140,7 +140,7 @@ client.connect((err) => {
 
     exports.getobjbyid = function (id) {
        return new Promise(function (resolve, reject) {
-                          client.query("SELECT unnest(objectid) FROM objetivoscompletos WHERE utilizadorid = $1",[id],
+                          client.query("select array_agg(c) from (select unnest(objectid) from objetivoscompletos WHERE utilizadorid = $1) as dt(c);",[id],
                           function (err, result) {
                                     if (err) {
                                         reject(err);
