e50a790dd999c052b76034e66ae8dd4f9d272634
Joel Dinis <joeldinisei1@gmail.com>
2017-08-02 22:26:26 +0200

    stuff

--- models/lowdatabase.js
+++ models/lowdatabase.js
@@ -155,6 +155,7 @@ client.connect((err) => {
 
         exports.getimgbyid = function (id) {
        return new Promise(function (resolve, reject) {
+                          client.query("SELECT local.image,local.description FROM objetivoscompletos,objetivos,local WHERE utilizadorid = $1 and objetivos.id = objetivoscompletos.objectid and local.id = objetivos.localid",[id],
                           function (err, result) {
                                     if (err) {
                                         reject(err);
