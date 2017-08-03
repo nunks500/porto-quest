51418fb89294b167aeadda20b5426fca4af52d8e
Joel Dinis <joeldinisei1@gmail.com>
2017-08-03 20:26:20 +0200

    more

--- models/lowdatabase.js
+++ models/lowdatabase.js
@@ -170,7 +170,7 @@ client.connect((err) => {
 
             exports.getnotobjbyids = function (id) {
        return new Promise(function (resolve, reject) {
-                          client.query("SELECT lat,long,objetivos.id as lng FROM objetivos,local WHERE local.id = objetivos.localid EXCEPT SELECT lat,long as lng FROM objetivoscompletos,objetivos,local WHERE utilizadorid = $1 and objetivos.id = objetivoscompletos.objectid and local.id = objetivos.localid",[id],
+                          client.query("SELECT lat,long as lng,objetivos.id FROM objetivos,local WHERE local.id = objetivos.localid EXCEPT SELECT lat,long as lng,objetivos.id  FROM objetivoscompletos,objetivos,local WHERE utilizadorid = $1 and objetivos.id = objetivoscompletos.objectid and local.id = objetivos.localid",[id],
                           function (err, result) {
                                     if (err) {
                                         reject(err);
